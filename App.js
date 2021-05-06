87babc762ac1112c8441a54c8e4f5c41ddf7f1ab
ntmanh90 <>
2021-05-06 10:33:48 +0700

    manh

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app! cập nhật app js </Text>
+      <Text> bổ xung cho nhánh develop 2 333</Text>
       <StatusBar style="auto" />
     </View>
   );
